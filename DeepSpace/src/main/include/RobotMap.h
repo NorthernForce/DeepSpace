20f5d29b5a5af4262bb280328be0f6cb3c2a283d
Joe Gaudet <gaudetja@gmail.com>
2019-01-28 19:11:52 -0500

    forgot

--- DeepSpace/src/main/include/RobotMap.h
+++ DeepSpace/src/main/include/RobotMap.h
@@ -1,3 +1,4 @@
+<<<<<<< Updated upstream
 /*----------------------------------------------------------------------------*/
 /* Copyright (c) 2017-2018 FIRST. All Rights Reserved.                        */
 /* Open Source Software - may be modified and shared by FRC teams. The code   */
@@ -25,3 +26,37 @@
 // constexpr int kRangeFinderModule = 1;
 const static int k_leftWCDrive_ID   = 1;
 const static int k_rightWCDrive_ID  = 2;
+const static int k_xbox_controller_id = 0;
+=======
+/*----------------------------------------------------------------------------*/
+/* Copyright (c) 2017-2018 FIRST. All Rights Reserved.                        */
+/* Open Source Software - may be modified and shared by FRC teams. The code   */
+/* must be accompanied by the FIRST BSD license file in the root directory of */
+/* the project.                                                               */
+/*----------------------------------------------------------------------------*/
+
+#pragma once
+#include <memory>
+// #include "WPI_TalonSRX.h"
+#include "ctre/Phoenix.h"
+
+/**
+ * The RobotMap is a mapping from the ports sensors and actuators are wired into
+ * to a variable name. This provides flexibility changing wiring, makes checking
+ * the wiring easier and significantly reduces the number of magic numbers
+ * floating around.
+ */
+
+// For example to map the left and right motors, you could define the
+// following variables to use with your drivetrain subsystem.
+// constexpr int kLeftMotor = 1;
+// constexpr int kRightMotor = 2;
+
+std::shared_ptr<WPI_TalonSRX> RobotMap::driveTrainTalonSRX1;
+std::shared_ptr<WPI_TalonSRX> RobotMap::driveTrainTalonSRX3;
+
+// If you are using multiple modules, make sure to define both the port
+// number and the module. For example you with a rangefinder:
+// constexpr int kRangeFinderPort = 1;
+// constexpr int kRangeFinderModule = 1;
+>>>>>>> Stashed changes
