ff6f63a27684ce44f6d9fe385bb94ae5bb32b3df
smatsumo2019 <smatsumo2019@falmouthschools.org>
2019-02-16 11:32:15 -0500

    Added PCM ID to Solenoids

--- DeepSpace/src/main/cpp/subsystems/Claw.cpp
+++ DeepSpace/src/main/cpp/subsystems/Claw.cpp
@@ -8,8 +8,8 @@
 #include "subsystems/Claw.h"
 
 Claw::Claw() : Subsystem("Claw") {
-  m_clawSolenoid.reset(new frc::Solenoid(RobotMap::Claw::k_clawSolenoid));
-  m_raiseSolenoid.reset(new frc::Solenoid(RobotMap::Claw::k_raiseSolenoid));
+  m_clawSolenoid.reset(new frc::Solenoid(RobotMap::PCM::k_pcm_id,RobotMap::Claw::k_clawSolenoid));
+  m_raiseSolenoid.reset(new frc::Solenoid(RobotMap::PCM::k_pcm_id,RobotMap::Claw::k_raiseSolenoid));
 
 
 }
