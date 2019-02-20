c9ca3308cf3ea480ad77de3e08902ead5d55240c
jcrosbyME <john-crosby@idexx.com>
2019-02-20 15:48:51 -0500

    BrushlessDrive: Added Open Loop Ramp

--- DeepSpace/src/main/cpp/subsystems/BrushlessDrive.cpp
+++ DeepSpace/src/main/cpp/subsystems/BrushlessDrive.cpp
@@ -50,6 +50,7 @@ void BrushlessDrive::ConfigureController(rev::CANSparkMax& controller)
   if(!controller.IsFollower())
   {
     controller.SetClosedLoopRampRate(k_rampRate);
+    controller.SetOpenLoopRampRate(k_rampRate);
   }
   controller.SetIdleMode(rev::CANSparkMax::IdleMode::kBrake);
 }
