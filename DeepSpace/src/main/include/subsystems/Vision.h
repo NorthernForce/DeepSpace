388c862ff2f04fc39e67cd017113e3119b50bb67
rpbritton <rpbritton@outlook.com>
2019-02-25 21:00:33 -0500

    Now resets white balance

--- DeepSpace/src/main/include/subsystems/Vision.h
+++ DeepSpace/src/main/include/subsystems/Vision.h
@@ -56,9 +56,10 @@ class Vision : public frc::Subsystem {
       "brightness=133,"
       "contrast=5,"
       "saturation=83,"
+      "white_balance_temperature_auto=0," // Must be manual to change settings.
+      "white_balance_temperature=4500,"
       "white_balance_temperature_auto=1,"
       "power_line_frequency=2,"
-      // "white_balance_temperature=4500," // Can't set this if it is auto! Check if can set it first.
       "sharpness=25,"
       "backlight_compensation=0,"
       "exposure_auto=1,"
