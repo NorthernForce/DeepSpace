88dfbfce442c9218fd7e6c959da71fe42189fd61
rpbritton <rpbritton@github.com>
2019-06-25 18:12:44 -0400

    the limitlit works

---
+++ DeepSpace/src/main/include/commands/VisionFollowLimelight.h
@@ -0,0 +1,37 @@
+/*----------------------------------------------------------------------------*/
+/* Copyright (c) 2017-2018 FIRST. All Rights Reserved.                        */
+/* Open Source Software - may be modified and shared by FRC teams. The code   */
+/* must be accompanied by the FIRST BSD license file in the root directory of */
+/* the project.                                                               */
+/*----------------------------------------------------------------------------*/
+
+#pragma once
+
+#include <frc/commands/Command.h>
+#include <frc/PIDController.h>
+
+#include <string>
+
+class VisionFollowLimelight : public frc::Command {
+ public:
+  VisionFollowLimelight();
+  void Initialize() override;
+  void Execute() override;
+  bool IsFinished() override;
+  void End() override;
+  void Interrupted() override;
+
+ private:
+  const static double k_p;
+  const static double k_i;
+  const static double k_d;
+
+  const static double k_maxTurnSpeed;
+
+  const static double k_targetOffset;
+
+  double m_error = 0;
+  double m_error_prior = 0;
+  double m_integral = 0;
+  double m_derivative = 0;
+};
